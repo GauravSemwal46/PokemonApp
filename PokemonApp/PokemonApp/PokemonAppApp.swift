36aeac67bdae2afbfad5869f85b8bc3bbe26a482
GauravSemwal46 <gauravsemwal6@gmail.com>
2025-03-18 09:06:34 +0530

    initial commit

---
+++ PokemonApp/PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Gaurav Semwal on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
